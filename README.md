2a93de73f247a3b25c6a85409a37fc4a8f69d905
Gerardo Nardelli <Gerardo Nardelli>
2016-01-05 19:09:46 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# RiteStats-Slack
+##RiteTag stats slash command  for Slack
+
+![slashimage](https://cloud.githubusercontent.com/assets/4614574/12128823/8f2bfef4-b3de-11e5-88ab-ed6ea8ce9446.png)
+
+
+1. Install via Composer
+
+  ```composer require ritetag/api:~2.0.0```
+
+2. Config RiteTag
+
+  Edit config.php with your credentials of RiteTag https://ritetag.com/developer/dashboard
+
+  ```
+  define('CONSUMER_KEY', '????')
+  define('CONSUMER_SECRET', '????')
+  define('OAUTH_TOKEN', "????")
+  define('OAUTH_TOKEN_SECRET',"????")
+  ```
+
+3. Config Slash Command On Slack
+
+![slack](https://cloud.githubusercontent.com/assets/4614574/12128827/953a8de2-b3de-11e5-9dca-98ca73b4be00.png)
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+const URL="https://ritetag.com/hashtag-stats/";
+$STATUS=[
+    0 => "*Underused*",
+    1 => "*Overused*",
+    2 => "*Long* *Life*",
+    3 => "*Hot* *Now*"
+];
+
+$command = $_POST['command'];
+$text = trim($_POST['text'],'#');
+$token = $_POST['token'];
+
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+
+require_once 'vendor/autoload.php';
+require_once 'config.php';
+
+$client = new \Ritetag\API\Client(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
+$body= json_decode($client->hashtagStats($text)->getBody());
+
+$tagStatus=$STATUS[$body->stats->color];
+$images=round($body->stats->images*100);
+$links=round($body->stats->links*100);
+$mentions=round($body->stats->mentions*100);
+
+$reply= '*[* '.$tagStatus.' *]* *#'.$body->stats->tag.'*'
+        .'  -  *Tweets* : '.$body->stats->tweets
+        .'  -  *Exposure* : '.$body->stats->exposure
+        .'  -  *ReTweets* : '.$body->stats->retweets
+        .'  -  *Images* : '.$images.'%'
+        .'  -  *Links* : '.$links.'%'
+        .'  -  *Mentions* : '.$mentions.'%'."\xA"
+        .'All values are per hour. '."\xA"
+        .'See full stats at '.URL.$text;
+
+echo $reply;
